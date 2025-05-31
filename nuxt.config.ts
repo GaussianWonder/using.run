7ff9c256a95e663d5f22fa3cb399a4351cd7f7d9
Virghileanu Teodor <teo.virghi@yahoo.ro>
2025-05-31 02:05:05 -0700

    chore: setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image']
-})
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    editorSupport: true,
+    viewer: true,
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full flex items-center bg-yellow-200">
+    <div class="font-bold text-3xl text-neutral-800">
+      Hello World!
+    </div>
+  </div>
+</template>
